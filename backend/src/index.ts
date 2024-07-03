7e36fa6434cde9d2bc8ab328fcfcb46300f52c98
Pierre Castro <pierre.castro89@gmail.com>
2024-07-03 11:10:25 +0200

    initialized backend with apollo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,58 @@
 import "reflect-metadata";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+const start = async () => {
+  const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+
+  const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
+
+  // The ApolloServer constructor requires two parameters: your schema
+  // definition and your set of resolvers.
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  // Passing an ApolloServer instance to the `startStandaloneServer` function:
+  //  1. creates an Express app
+  //  2. installs your ApolloServer instance as middleware
+  //  3. prepares your app to handle incoming requests
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  console.log(`🚀  Server ready at: ${url}`);
+}
+
+start();
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 
 function App() {
   return (
-    <div>Bonjour</div>
+    <div>Test</div>
   )
 }
 
