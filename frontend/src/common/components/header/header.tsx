e9aaabe1a74db46da9dbdcecb4f655ea594ced46
Pierre Castro <pierre.castro89@gmail.com>
2024-07-04 11:55:32 +0200

    fixed css convention

--- frontend/src/common/components/header/header.tsx
+++ frontend/src/common/components/header/header.tsx
@@ -1,24 +1,24 @@
 import uraliveLogo from '@assets/uralive-logo.png';
-import styles from './header.module.scss';
+import './header.scss';
 import { useState } from 'react';
 import { AiOutlineUser } from "react-icons/ai";
 
 const Header = () => {
   const { userLoggedIn, setUserLoggedIn } = useState(false);
   return (
-    <header className={styles["navbar"]}>
+    <header className="navbar">
       {/* TODO: Add a link to the home page */}
-      <div className={styles["logo"]}>
-        <img src={uraliveLogo} className={styles["logo-img"]} alt="Uralive" />
-        <div className={styles["logo-txt"]}>UR<span className={styles["green"]}>a</span>L<span className={styles["green"]}>ive</span></div>
+      <div className="navbar__logo">
+        <img src={uraliveLogo} className="navbar__logo-img" alt="Uralive" />
+        <div className="navbar__logo-txt">UR<span className="navbar__green">a</span>L<span className="navbar__green">ive</span></div>
       </div>
       {/* Prévoir un composant menu qui s'adapte en fonction de la connexion utilisateur (ou pas) */}
-      <div className={styles["menu"]}>
-        <div className={styles["menu-item"]}>
+      <div className="navbar__menu">
+        <div className="navbar__menu-item">
           <span>
             Login
           </span>
-          <AiOutlineUser className={styles["icon"]} size={20} />
+          <AiOutlineUser className="navbar__icon" size={20} />
         </div>
       </div>
     </header>
