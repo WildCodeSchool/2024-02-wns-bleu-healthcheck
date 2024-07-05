4fb1473fbf0c91b7e2df1b0c364164df2132ba21
Scaramax <scaramax@hotmail.fr>
2024-07-05 12:08:04 +0200

    Added date to gql query

--- frontend/src/common/graphql/queries.ts
+++ frontend/src/common/graphql/queries.ts
@@ -3,6 +3,7 @@ import { gql } from "@apollo/client";
 export const TEST_URL = gql`
 query TestUrl($url: String!) {
     testUrl(url: $url) {
+        date
         response_time
         status
         status_code
