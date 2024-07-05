e89c7565ee7132cc265d4573cd84758c1dd1de2a
Pierre Castro <pierre.castro89@gmail.com>
2024-07-05 12:32:41 +0200

    refactored some code

---
+++ frontend/src/common/components/UrlCard/UrlCard.tsx
@@ -0,0 +1,43 @@
+import { BsCalendar } from "react-icons/bs";
+import { GoLink, GoSync } from "react-icons/go";
+import "./_urlCard.scss"
+
+export interface UrlData {
+  url: string;
+  _id?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  frequency?: number;
+  name?: string;
+  lastStatus: {
+    date: Date;
+    response_time: number;
+    status: number;
+    status_code: number;
+    status_message: string;
+  };
+}
+
+function UrlCard({ urlData }: { urlData: UrlData }) {
+  return (
+    <div className="card">
+      {urlData.name && (<p className="card__name">{urlData.name}</p>)}
+      <div className="card__content">
+        <ul className="card__list">
+          <li className="card__url card__element">
+            <span className="card__icon"><GoLink /></span>URL : {urlData.url}
+          </li>
+          <li className="card__statut card__element">
+            <span className="card__icon"><BsCalendar /></span>Statut : {urlData.lastStatus.status}
+          </li>
+          {urlData.frequency && (<li className="card__frequence card__element">
+            <span className="card__icon"><GoSync /></span>Fréquence : {urlData.frequency}
+          </li>)}
+        </ul>
+        <p className="card__lastquery">Dernière requête : {urlData.lastStatus.date.toDateString()}</p>
+      </div>
+    </div>
+  );
+}
+
+export default UrlCard;
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,14 +1,38 @@
 import TestBarUrl from "@/common/components/testBarUrl/TestBarUrl";
 import "./Home.scss";
+import UrlCard, { UrlData } from "@/common/components/UrlCard/UrlCard";
+import { useEffect, useState } from "react";
+import { useLazyQuery } from "@apollo/client";
+import { TEST_URL } from "@/common/graphql/queries";
 
 const Home = () => {
 
+  const [urlData, setUrlData] = useState<UrlData>();
+  const [executeQuery, { data }] = useLazyQuery(TEST_URL);
+
+  useEffect(() => {
+    if (data !== undefined) {
+      const { date, response_time, status, status_code, status_message } = data.testUrl;
+      setUrlData({
+        url: data.testUrl.url,
+        lastStatus: {
+          date,
+          response_time,
+          status,
+          status_code,
+          status_message,
+        }
+      });
+    }
+  }, [data]);
+
   return (
     <div className="home__content-wrapper">
-        <div className="home__content">
-            <h1 className="home__main-title">LA NOUVELLE ÈRE DU DASHBOARDING</h1>
-            <TestBarUrl/>
-        </div>
+      <div className="home__content">
+        <h1 className="home__main-title">LA NOUVELLE ÈRE DU DASHBOARDING</h1>
+        <TestBarUrl execute={executeQuery} />
+        {urlData && (<UrlCard urlData={urlData} />)}
+      </div>
     </div>
   );
 };
