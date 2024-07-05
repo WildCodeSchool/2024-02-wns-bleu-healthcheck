8b59e99ab073c14e950ed440a724847986947509
Scaramax <scaramax@hotmail.fr>
2024-07-05 13:27:03 +0200

    Typed testRequest response and fixed gql TestUrl query structure

--- frontend/src/common/graphql/queries.ts
+++ frontend/src/common/graphql/queries.ts
@@ -3,10 +3,13 @@ import { gql } from "@apollo/client";
 export const TEST_URL = gql`
 query TestUrl($url: String!) {
     testUrl(url: $url) {
-        date
-        response_time
-        status
-        status_code
-        status_message
+        url
+        lastStatus {
+            date
+            status
+            response_time
+            status_code
+            status_message
+        }
     }
 }`;
