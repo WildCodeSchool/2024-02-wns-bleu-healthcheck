a36529e5e4829e50a9b2fad90224bd350b075d72
Ghislain Pellicano <g.pellicano@aiasports.fr>
2024-07-10 10:24:05 +0200

    added sign in modal template

--- frontend/src/common/components/loginModal/LoginModal.tsx
+++ frontend/src/common/components/loginModal/LoginModal.tsx
@@ -1,15 +1,34 @@
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
 import "./LoginModal.scss";
-import { Link } from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 interface LoginModalProps {
   open: boolean;
   handleClose: () => void;
 }
+const defaultTheme = createTheme();
 
 export default function LoginModal({ open, handleClose }: LoginModalProps) {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
   return (
     <div className="login__modal">
       <Modal
@@ -19,30 +38,77 @@ export default function LoginModal({ open, handleClose }: LoginModalProps) {
         aria-describedby="modal-modal-form"
       >
         <Box className="login__modal-box">
-          <h2 id="modal-modal-title" className="login__modal-title">
-            Se connecter
-          </h2>
-          <div id="modal-modal-form" className="login__modal-form-container">
-            <form className="login__modal-form">
-              <label htmlFor="email">Email</label>
-              <input type="email" id="email" name="email" required />
-              <label htmlFor="password">Mot de passe</label>
-              <input type="password" id="password" name="password" required />
-              <Button type="submit" className="login__modal-form-button">
-                Se connecter
-              </Button>
-            </form>
-            <Link to="/register">
-              <Button
-                type="submit"
-                className="login__modal-form-button"
-                onClick={handleClose}
+          <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+              <CssBaseline />
+              <Box
+                sx={{
+                  marginTop: 8,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
               >
-                Créer un compte
-              </Button>
-            </Link>
-          </div>
-          <Button onClick={handleClose}>Fermer</Button>
+                <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  Se connecter
+                </Typography>
+                <Box
+                  component="form"
+                  onSubmit={handleSubmit}
+                  noValidate
+                  sx={{ mt: 1 }}
+                >
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Adresse mail"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                  />
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Mot de passe"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox value="remember" color="primary" />}
+                    label="Se souvenir de moi"
+                  />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Se connecter
+                  </Button>
+                  <Grid container>
+                    <Grid item xs>
+                      <Link href="#" variant="body2">
+                        Mot de passe oublié ?
+                      </Link>
+                    </Grid>
+                    <Grid item>
+                      <Link href="#" variant="body2">
+                        {"Pas de compte ? S'inscrire"}
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Box>
+            </Container>
+          </ThemeProvider>
         </Box>
       </Modal>
     </div>
