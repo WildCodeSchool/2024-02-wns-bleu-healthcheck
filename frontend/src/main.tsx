ebb5c7127d00b673e055b7baaaa7e5f40cd7c431
Ghislain Pellicano <127127639+ghispell@users.noreply.github.com>
2024-07-11 16:24:48 +0200

    Toastify setup (#57)
    added toastify package
    
    Co-authored-by: Ghislain Pellicano <g.pellicano@aiasports.fr>
    Co-authored-by: William <scaramax@hotmail.fr>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router.tsx";
 import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from "@apollo/client";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const link = createHttpLink({
   uri: '/api',
@@ -17,6 +19,7 @@ export const client = new ApolloClient({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
+      <ToastContainer />
       <RouterProvider router={router} />
     </ApolloProvider>
   </React.StrictMode>
