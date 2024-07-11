820004c7e0a0a08bf99f79ccee23943edd9fc61f
Scaramax <scaramax@hotmail.fr>
2024-07-11 12:26:07 +0200

    Dashboard first iteration + modified urlCard max size

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,37 @@
+import "./Dashboard.scss"
+import {GET_SAVED_QUERIES} from "@/common/graphql/queries.ts";
+import { useQuery} from "@apollo/client";
+import UrlCard, {UrlData} from "../../common/components/UrlCard/UrlCard.tsx";
+
 const Dashboard = () => {
-  return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
-  );
+
+    const { data, loading, error } = useQuery(GET_SAVED_QUERIES);
+    const savedQueries:UrlData[] = data?.getSavedQueries;
+
+
+    if(error) {
+        return <p>Error: {error.message}</p>
+    }
+    return (
+        <div className="dashboard__wrapper">
+            <div className="dashboard_header">
+                Add stuff here (header)
+
+            </div>
+            <div className="dashboard__body">
+                {loading &&
+                    <div className="dashboard__loading">
+                        Loading...
+                    </div>
+                }
+
+                {savedQueries && savedQueries.map((query:UrlData) => {
+                    return <UrlCard urlData={query} key={query._id} />
+                })}
+            </div>
+
+        </div>
+    );
 };
 
 export default Dashboard;
