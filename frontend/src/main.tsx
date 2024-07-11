2905b332906a4b6d2428efb21f880a46cb0d0073
Ghislain Pellicano <g.pellicano@aiasports.fr>
2024-07-11 15:22:31 +0200

    added toastify package

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router.tsx";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const client = new ApolloClient({
   uri: "/api",
@@ -12,6 +14,7 @@ const client = new ApolloClient({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
+      <ToastContainer />
       <RouterProvider router={router} />
     </ApolloProvider>
   </React.StrictMode>
