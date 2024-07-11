90093220d87f0c44a5ff610eaec2156de2afff81
Scaramax <scaramax@hotmail.fr>
2024-07-11 15:19:05 +0200

    Handled errors when there is no log available yet for a saved query

--- frontend/src/common/components/UrlCard/UrlCard.tsx
+++ frontend/src/common/components/UrlCard/UrlCard.tsx
@@ -10,7 +10,7 @@ export interface UrlData {
   updatedAt?: string;
   frequency?: number;
   name?: string;
-  lastStatus: {
+  lastStatus?: {
     date: string;
     response_time: number;
     status: number;
@@ -20,7 +20,7 @@ export interface UrlData {
 }
 
 function UrlCard({ urlData }: { urlData: UrlData }) {  return (
-    <div className={`card ${urlData.lastStatus.status === 2 ? "success" : urlData.lastStatus.status === 1 ? "warning" : "error"}`}>
+    <div className={`card ${urlData.lastStatus?.status === 2 ? "success" : urlData.lastStatus?.status === 1 ? "warning" : "error"}`}>
       {urlData.name && (<p className="card__name">{urlData.name}</p>)}
       <div className="card__content">
         <ul className="card__list">
@@ -28,16 +28,16 @@ function UrlCard({ urlData }: { urlData: UrlData }) {  return (
             <span className="card__icon"><GoLink /></span>URL : {urlData.url}
           </li>
           <li className="card__url card__element">
-            <span className="card__icon"><GoCode /></span>Code de retour : {urlData.lastStatus.status_code}
+            <span className="card__icon"><GoCode /></span>Code de retour : {urlData.lastStatus?.status_code}
           </li>
           <li className="card__url card__element">
-            <span className="card__icon"><CiTimer/></span>Temps de réponse : {urlData.lastStatus.response_time} ms
+            <span className="card__icon"><CiTimer/></span>Temps de réponse : {urlData.lastStatus?.response_time ? `${urlData.lastStatus?.response_time} ms` : ""}
           </li>
           {urlData.frequency && (<li className="card__frequence card__element">
             <span className="card__icon"><GoSync /></span>Fréquence : {urlData.frequency}
           </li>)}
         </ul>
-        <p className="card__lastquery">Dernière requête : {moment(urlData.lastStatus.date).fromNow()}</p>
+        <p className="card__lastquery">Dernière requête : {urlData.lastStatus?.date ? moment(urlData.lastStatus?.date).fromNow() : "en cours" }</p>
       </div>
     </div>
   );
