9f5d54cb2dac7b67bd884a2c171d2ed8d2203f5a
Ghislain Pellicano <g.pellicano@aiasports.fr>
2024-07-12 10:58:07 +0200

    url format check on testbarurl and savequerybarurl

--- frontend/src/common/components/saveQueryBarUrl/SaveQueryBarUrl.tsx
+++ frontend/src/common/components/saveQueryBarUrl/SaveQueryBarUrl.tsx
@@ -1,21 +1,30 @@
 import React, { useState } from "react";
 import "./SaveQueryBarUrl.scss";
 import { AiOutlineGlobal } from "react-icons/ai";
-import {useMutation} from "@apollo/client";
-import {CREATE_SAVED_QUERY} from "@/common/graphql/queries";
+import { useMutation } from "@apollo/client";
+import { CREATE_SAVED_QUERY } from "@/common/graphql/queries";
+import Tooltip from "@mui/material/Tooltip";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const SaveQueryBarUrl = () => {
-
   const [url, setUrl] = useState("");
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUrl(e.target.value);
+  const [isValidUrl, setIsValidUrl] = useState(false);
+
+  const validateUrl = (inputUrl: string) => {
+    const pattern =
+      /^(https?:\/\/)([\w\-]+(\.[\w\-]+)*)(\.(com|fr|net|org|io|co|us|uk|de|jp|cn|ru|br|au|in|it|es|ca|se|no|fi|dk|nl|pl|ph|id|nz|gr|ar|mx|sg|il|my|tr|pt|cz|hk|ie|kr|th|at|be|ch|vn|za|eg|pk|ng|bd|ua|pe|ro|sa|tw|cl|ve|ir|ma|ec|dz|ke|np|lk|kz|tn|gt|do|by|az|sv|zw|gh|tz|ug|sn|kh|mm|zm|ao|rw|cm|ci|mw|am|mg|bf|ne|mo|la|bj|tg|pg|ni|sl|fj|bw|lr|gq|bb|bs|mv|bn|is))(\b(\/[\w\-]*)*(\/[\w\-.,@?^=%&:~+#]*)?)?$/;
+
+    return pattern.test(inputUrl);
   };
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputUrl = e.target.value;
+    setUrl(inputUrl);
+    setIsValidUrl(validateUrl(inputUrl));
+  };
   const [createSavedQuery] = useMutation(CREATE_SAVED_QUERY);
 
   const handleSubmit = () => {
-
     // Remove the protocol and "www." prefix if they exist
     const cleanedUrl = url.replace(/^(https?:\/\/)?(www\.)?/, "");
 
@@ -28,24 +37,35 @@ const SaveQueryBarUrl = () => {
     createSavedQuery({ variables: { data: { url, name, frequency } } });
   };
 
-return (
-  <>
-    <div className="savequery__bar">
-      <input
-        name="savequery__bar"
-        placeholder="Sauvegarder une URL..."
-        value={url}
-        onChange={handleInputChange}
-        className="savequery__bar-input"
-      />
-      <div className="savequery__bar-button">
-        <button onClick={handleSubmit}>
-          <AiOutlineGlobal />
-        </button>
+  return (
+    <>
+      <div className="savequery__bar">
+        <input
+          name="savequery__bar"
+          placeholder="Sauvegarder une URL..."
+          value={url}
+          onChange={handleInputChange}
+          className="savequery__bar-input"
+        />
+        <Tooltip
+          title={isValidUrl ? "Sauvegarder la requête" : "URL invalide"}
+          placement="top"
+        >
+          <div
+            className={
+              isValidUrl
+                ? "savequery__bar-button-valid"
+                : "savequery__bar-button-invalid"
+            }
+          >
+            <button onClick={handleSubmit} disabled={!isValidUrl}>
+              <AiOutlineGlobal />
+            </button>
+          </div>
+        </Tooltip>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
 };
 
 export default SaveQueryBarUrl;
--- frontend/src/common/components/testBarUrl/TestBarUrl.tsx
+++ frontend/src/common/components/testBarUrl/TestBarUrl.tsx
@@ -2,54 +2,57 @@ import React, { useState } from "react";
 import "./TestBarUrl.scss";
 import { AiOutlineGlobal } from "react-icons/ai";
 import { LazyQueryExecFunction, OperationVariables } from "@apollo/client";
+import Tooltip from "@mui/material/Tooltip";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const TestBarUrl = ({ execute }: { execute: LazyQueryExecFunction<any, OperationVariables> }) => {
-
+const TestBarUrl = ({
+  execute,
+}: {
+  execute: LazyQueryExecFunction<any, OperationVariables>;
+}) => {
   const [url, setUrl] = useState("");
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUrl(e.target.value);
-  };
+  const [isValidUrl, setIsValidUrl] = useState(false);
 
+  const validateUrl = (inputUrl: string) => {
+    const pattern =
+      /^(https?:\/\/)([\w\-]+(\.[\w\-]+)*)(\.(com|fr|net|org|io|co|us|uk|de|jp|cn|ru|br|au|in|it|es|ca|se|no|fi|dk|nl|pl|ph|id|nz|gr|ar|mx|sg|il|my|tr|pt|cz|hk|ie|kr|th|at|be|ch|vn|za|eg|pk|ng|bd|ua|pe|ro|sa|tw|cl|ve|ir|ma|ec|dz|ke|np|lk|kz|tn|gt|do|by|az|sv|zw|gh|tz|ug|sn|kh|mm|zm|ao|rw|cm|ci|mw|am|mg|bf|ne|mo|la|bj|tg|pg|ni|sl|fj|bw|lr|gq|bb|bs|mv|bn|is))(\b(\/[\w\-]*)*(\/[\w\-.,@?^=%&:~+#]*)?)?$/;
+
+    return pattern.test(inputUrl);
+  };
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputUrl = e.target.value;
+    setUrl(inputUrl);
+    setIsValidUrl(validateUrl(inputUrl));
+  };
 
   const handleSubmit = () => {
     execute({ variables: { url } });
   };
 
-return (
-  <>
-    <div className="test__bar">
-      <input
-        name="test__bar"
-        placeholder="Tester une URL..."
-        value={url}
-        onChange={handleInputChange}
-        className="test__bar-input"
-      />
-      <div className="test__bar-button">
-        <button onClick={handleSubmit}>
-          <AiOutlineGlobal size={22}/>
-        </button>
-      </div>
-    </div>
-
-    {/* <div>
-        {loading && <p>Chargement...</p>}
-        {error && <p>Erreur : {error.message}</p>}
-        {data !== undefined && (
-          <div>
-            Résultat :
-            <div>
-              <p>Temps de réponse : {responseTime}ms</p>
-              <p>Status : {status}</p>
-              <p>Code de statut : {statusCode}</p>
-              <p>Message de statut : {statusMessage}</p>
-            </div>
+  return (
+    <>
+      <div className="test__bar">
+        <input
+          name="test__bar"
+          placeholder="Tester une URL..."
+          value={url}
+          onChange={handleInputChange}
+          className="test__bar-input"
+        />
+        <Tooltip title={isValidUrl ? "Tester l'URL !" : "URL invalide"}>
+          <div
+            className={
+              isValidUrl ? "test__bar-button-valid" : "test__bar-button-invalid"
+            }
+          >
+            <button onClick={handleSubmit} disabled={!isValidUrl}>
+              <AiOutlineGlobal size={22} />
+            </button>
           </div>
-        )}
-      </div> */}
-  </>
-);
+        </Tooltip>
+      </div>
+    </>
+  );
 };
 
 export default TestBarUrl;
