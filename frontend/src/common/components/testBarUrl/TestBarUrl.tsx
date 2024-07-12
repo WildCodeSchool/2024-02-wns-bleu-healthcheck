08d7a763af0e99924d97ae1f3ea1d8d5dfc8ad4f
Ghislain Pellicano <g.pellicano@aiasports.fr>
2024-07-12 12:23:48 +0200

    refacto validate url function

--- frontend/src/common/components/testBarUrl/TestBarUrl.tsx
+++ frontend/src/common/components/testBarUrl/TestBarUrl.tsx
@@ -3,6 +3,7 @@ import "./TestBarUrl.scss";
 import { AiOutlineGlobal } from "react-icons/ai";
 import { LazyQueryExecFunction, OperationVariables } from "@apollo/client";
 import Tooltip from "@mui/material/Tooltip";
+import useValidateUrl from "@/common/hooks/useValidateUrl";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const TestBarUrl = ({
@@ -11,18 +12,11 @@ const TestBarUrl = ({
   execute: LazyQueryExecFunction<any, OperationVariables>;
 }) => {
   const [url, setUrl] = useState("");
-  const [isValidUrl, setIsValidUrl] = useState(false);
+  const isValidUrl = useValidateUrl(url);
 
-  const validateUrl = (inputUrl: string) => {
-    const pattern =
-      /^(https?:\/\/)([\w\-]+(\.[\w\-]+)*)(\.(com|fr|net|org|io|co|us|uk|de|jp|cn|ru|br|au|in|it|es|ca|se|no|fi|dk|nl|pl|ph|id|nz|gr|ar|mx|sg|il|my|tr|pt|cz|hk|ie|kr|th|at|be|ch|vn|za|eg|pk|ng|bd|ua|pe|ro|sa|tw|cl|ve|ir|ma|ec|dz|ke|np|lk|kz|tn|gt|do|by|az|sv|zw|gh|tz|ug|sn|kh|mm|zm|ao|rw|cm|ci|mw|am|mg|bf|ne|mo|la|bj|tg|pg|ni|sl|fj|bw|lr|gq|bb|bs|mv|bn|is))(\b(\/[\w\-]*)*(\/[\w\-.,@?^=%&:~+#]*)?)?$/;
-
-    return pattern.test(inputUrl);
-  };
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const inputUrl = e.target.value;
     setUrl(inputUrl);
-    setIsValidUrl(validateUrl(inputUrl));
   };
 
   const handleSubmit = () => {
