068d5ecea4b3b3cee08f5e80a82ed12bfb8461a8
Ghislain Pellicano <127127639+ghispell@users.noreply.github.com>
2024-08-23 11:54:45 +0200

    Ajout d'un message pour inciter un utilisateur gratuit à se créer un compte (#80)
    * added message to invite free user to create account
    
    * added condition if userr is connected
    
    * added map
    
    * removed unsued component
    
    ---------
    
    Co-authored-by: Ghislain Pellicano <g.pellicano@aiasports.fr>

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -4,12 +4,16 @@ import UrlCard, { UrlData } from "@/common/components/UrlCard/UrlCard";
 import { useEffect, useState } from "react";
 import { useLazyQuery } from "@apollo/client";
 import { TEST_URL } from "@/common/graphql/queries";
+import { Link } from "react-router-dom";
+import useAuth from "@/common/hooks/useAuth";
 
 const Home = () => {
   const [urlData, setUrlData] = useState<UrlData>();
   const [executeQuery, { data }] = useLazyQuery(TEST_URL, {
     fetchPolicy: "network-only",
   });
+  const text = "Créez-vous un compte ";
+  const { userInfos } = useAuth();
   useEffect(() => {
     if (data !== undefined) {
       const { date, response_time, status, status_code, status_message } =
@@ -34,7 +38,25 @@ const Home = () => {
         <h1 className="home__main-title">LA NOUVELLE ÈRE DU DASHBOARDING</h1>
         <TestBarUrl execute={executeQuery} />
       </div>
-      {urlData && <UrlCard urlData={urlData} />}
+      {urlData && (
+        <>
+          {" "}
+          <UrlCard urlData={urlData} />
+          {!userInfos.isLoggedIn && (
+            <div className="home__redirect">
+              <h2>
+                Vous voulez enregistrer cette URL?{" "}
+                <Link to="/register" className="home__redirect-link">
+                  {text.split("").map((char, index) => (
+                    <span key={index}>{char}</span>
+                  ))}
+                </Link>
+                et profitez d'un dashboard complet !
+              </h2>
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 };
