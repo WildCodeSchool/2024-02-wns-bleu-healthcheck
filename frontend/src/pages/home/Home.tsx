2410b8cf054e4832093c5dc7144c40e4c014ad70
Ghislain Pellicano <g.pellicano@aiasports.fr>
2024-08-23 09:34:48 +0200

    added condition if userr is connected

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -5,12 +5,14 @@ import { useEffect, useState } from "react";
 import { useLazyQuery } from "@apollo/client";
 import { TEST_URL } from "@/common/graphql/queries";
 import { Link } from "react-router-dom";
+import useAuth from "@/common/hooks/useAuth";
 
 const Home = () => {
   const [urlData, setUrlData] = useState<UrlData>();
   const [executeQuery, { data }] = useLazyQuery(TEST_URL, {
     fetchPolicy: "network-only",
   });
+  const { userInfos } = useAuth();
   useEffect(() => {
     if (data !== undefined) {
       const { date, response_time, status, status_code, status_message } =
@@ -39,35 +41,37 @@ const Home = () => {
         <>
           {" "}
           <UrlCard urlData={urlData} />
-          <div className="home__redirect">
-            <h2>
-              Vous voulez enregistrer cette URL?{" "}
-              <Link to="/register" className="home__redirect-link">
-                <span>C</span>
-                <span>r</span>
-                <span>é</span>
-                <span>e</span>
-                <span>z</span>
-                <span>-</span>
-                <span>v</span>
-                <span>o</span>
-                <span>u</span>
-                <span>s</span>
-                <span> </span>
-                <span>u</span>
-                <span>n</span>
-                <span> </span>
-                <span>c</span>
-                <span>o</span>
-                <span>m</span>
-                <span>p</span>
-                <span>t</span>
-                <span>e</span>
-                <span> </span>
-              </Link>
-              et profitez d'un dashboard complet !
-            </h2>
-          </div>
+          {!userInfos.isLoggedIn && (
+            <div className="home__redirect">
+              <h2>
+                Vous voulez enregistrer cette URL?{" "}
+                <Link to="/register" className="home__redirect-link">
+                  <span>C</span>
+                  <span>r</span>
+                  <span>é</span>
+                  <span>e</span>
+                  <span>z</span>
+                  <span>-</span>
+                  <span>v</span>
+                  <span>o</span>
+                  <span>u</span>
+                  <span>s</span>
+                  <span> </span>
+                  <span>u</span>
+                  <span>n</span>
+                  <span> </span>
+                  <span>c</span>
+                  <span>o</span>
+                  <span>m</span>
+                  <span>p</span>
+                  <span>t</span>
+                  <span>e</span>
+                  <span> </span>
+                </Link>
+                et profitez d'un dashboard complet !
+              </h2>
+            </div>
+          )}
         </>
       )}
     </div>
