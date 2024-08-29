10b647171551048adf8755c8a419c3040c703300
Scaramax <scaramax@hotmail.fr>
2024-08-29 15:54:50 +0200

    Logs fetched only when clicking on a urlCard and used as props to LogsModal

---
+++ frontend/src/common/models/Log.ts
@@ -0,0 +1,8 @@
+export interface Log {
+    _id: string;
+    date: string;
+    response_time: number;
+    status: number;
+    status_code: number;
+    status_message: string;
+}
--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,21 +1,26 @@
 import "./Dashboard.scss"
-import {GET_SAVED_QUERIES} from "@/common/graphql/queries.ts";
+import {GET_LOGS, GET_SAVED_QUERIES} from "@/common/graphql/queries.ts";
 import {useQuery} from "@apollo/client";
 import UrlCard, {UrlData} from "../../common/components/UrlCard/UrlCard.tsx";
 import {CircularProgress} from "@mui/material";
 import SaveQueryBarUrl from "@/common/components/saveQueryBarUrl/SaveQueryBarUrl.tsx";
 import {useState, useEffect} from "react";
 import LogsModal from "@/common/components/logsModal/LogsModal.tsx";
+import {UrlDataWithLogs} from "@/common/models/UrlDataWithLogs.ts";
 
 const Dashboard = () => {
 
     const [savedQueries, setSavedQueries] = useState<UrlData[]>([]);
-    const [selectedQuery, setSelectedQuery] = useState<UrlData | null>(null);
+    const [selectedQuery, setSelectedQuery] = useState<UrlDataWithLogs | null>(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const {data, loading, error} = useQuery(GET_SAVED_QUERIES, {
         pollInterval: 60000, // Refetch every 60 seconds
         fetchPolicy: "cache-and-network"
+    });
+
+    const { refetch } = useQuery(GET_LOGS, {
+        skip: true,  // Don't fetch automatically, the logs will be fetched only when clicking on a card
     });
 
     useEffect(() => {
@@ -24,8 +29,9 @@ const Dashboard = () => {
         }
     }, [data]);
 
-    const handleCardClick = (query: UrlData) => {
-        setSelectedQuery(query);
+    const handleCardClick = async (query: UrlData) => {
+        const { data } = await refetch({ savedQueryId: query._id });
+        setSelectedQuery({ ...query, logs: data?.getLogsForSavedQuery || [] });
         setIsModalOpen(true);
     };
 
@@ -56,7 +62,7 @@ const Dashboard = () => {
                 })}
             </div>
 
-            <LogsModal open={isModalOpen} handleClose={handleCloseModal} urlData={selectedQuery} />
+            <LogsModal open={isModalOpen} handleClose={handleCloseModal} urlData={selectedQuery} logs={selectedQuery?.logs || []} />
         </div>
     );
 };
