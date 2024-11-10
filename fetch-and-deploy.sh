8d20626114a97801f0535de10e9083ee06ce4e48
Pierre Castro <pierre.castro89@gmail.com>
2024-11-10 03:16:24 +0100

    fix: Update script

--- fetch-and-deploy.sh
+++ fetch-and-deploy.sh
@@ -1,18 +1,11 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 
-if [ $1 = "dev" ]; then
-  docker pull pierrecastro/healthcheck-frontend:dev
-  docker pull pierrecastro/healthcheck-backend:dev
-  docker compose --env-file .env.dev -f docker-compose.yml down -d
-  docker compose --env-file .env.dev -f docker-compose.yml up -d
-  exit 0
-elif [ $1 = "prod" ]; then
-  docker pull pierrecastro/healthcheck-frontend:prod
-  docker pull pierrecastro/healthcheck-backend:prod
-  docker compose --env-file .env.prod -f docker-compose.yml down -d
-  docker compose --env-file .env.prod -f docker-compose.yml up -d
-  exit 0
-fi
+docker pull pierrecastro/healthcheck-frontend:dev
+docker pull pierrecastro/healthcheck-backend:dev
+docker pull pierrecastro/healthcheck-frontend:prod
+docker pull pierrecastro/healthcheck-backend:prod
 
-echo "Invalid argument. Please use 'dev' or 'prod'." >&2
-exit 1
+docker compose --env-file .env.dev -f docker-compose.yml down -d
+docker compose --env-file .env.prod -f docker-compose.yml down -d
+docker compose --env-file .env.dev -f docker-compose.yml up -d
+docker compose --env-file .env.prod -f docker-compose.yml up -d
