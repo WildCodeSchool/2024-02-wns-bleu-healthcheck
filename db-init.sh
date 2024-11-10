9d6162bb844e056ae883fd86f8bcf1f9787f5274
Pierre Castro <pierre.castro89@gmail.com>
2024-11-10 02:03:47 +0100

    chore: Rework deployment process

---
+++ db-init.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+
+# First, we need to check if the database exists.
+until pg_isready; do
+    echo "Waiting for postgres..."
+    sleep 2
+done
+echo "Creating database $DATABASE_NAME..."; \
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -c "CREATE DATABASE $DATABASE_NAME"
