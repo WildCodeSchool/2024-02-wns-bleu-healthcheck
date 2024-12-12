78753c72f503826e4f30be4fc227a8a97c0426a7
Scaramax <scaramax@hotmail.fr>
2024-12-12 17:04:37 +0100

    Done : Premium Page, Menu

--- frontend/src/common/components/header/header-menu/HeaderMenu.tsx
+++ frontend/src/common/components/header/header-menu/HeaderMenu.tsx
@@ -1,6 +1,7 @@
 import useAuth from "@/common/hooks/useAuth";
 import "./HeaderMenu.scss";
-import { Button } from "@mui/material";
+import {Button, IconButton, SwipeableDrawer} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 
 import { Link, useNavigate } from "react-router-dom";
 import { useLazyQuery } from "@apollo/client";
@@ -9,6 +10,8 @@ import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined
 import MonitorHeartOutlinedIcon from '@mui/icons-material/MonitorHeartOutlined';
 import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import {useEffect, useState} from "react";
+import Box from "@mui/material/Box";
 
 
 interface IHeaderMenu {
@@ -27,51 +30,122 @@ const HeaderMenu = ({ handleOpen }: IHeaderMenu) => {
   });
   const { userInfos, refetch } = useAuth();
 
-  return (
-    <div className="header__menu">
-      <div className="header__menu-item">
-        {userInfos.isLoggedIn ? (
-          <>
-            {userInfos.role && userInfos.role < 1 && (
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const [sidebarDisplayed, setSidebarDisplayed] = useState(false);
+  const toggleDrawer = (open: boolean) => {
+    setSidebarDisplayed(open);
+  };
+
+  return isSmallScreen ?
+    (
+      <>
+        <div onClick={() => toggleDrawer(true)}>
+          <IconButton>
+            <MenuIcon/>
+          </IconButton>
+        </div>
+        <SwipeableDrawer
+          anchor='right'
+          open={sidebarDisplayed}
+          onClose={() => toggleDrawer(false)}
+          onOpen={() => toggleDrawer(true)}
+        >
+          <Box sx={{width: 200}} role="presentation" onClick={() => toggleDrawer(false)}>
+            <div className="header__drawer-container">
+              {userInfos.role && userInfos.role < 1 && (
+                <Button
+                  component={Link}
+                  to="/premium"
+                  className="header__drawer-button"
+                >
+                  <span>Premium</span>
+                  <StarBorderOutlinedIcon/>
+                </Button>
+              )}
               <Button
                 component={Link}
-                to="/premium"
+                to="/dashboard"
+                className="header__drawer-button"
+              >
+                <span>Dashboard</span>
+                <MonitorHeartOutlinedIcon/>
+              </Button>
+              <Button
+                component={Link}
+                to="/settings"
+                className="header__drawer-button"
+              >
+                <span>{userInfos.name}</span>
+                <AccountCircleOutlinedIcon/>
+              </Button>
+              <Button onClick={() => logout()} className="header__drawer-button">
+                <span>Logout</span>
+                <LogoutOutlinedIcon/>
+              </Button>
+            </div>
+          </Box>
+        </SwipeableDrawer>
+      </>
+    )
+    :
+    (
+      <div className="header__menu">
+        <div className="header__menu-item">
+          {userInfos.isLoggedIn ? (
+            <>
+              {userInfos.role && userInfos.role < 1 && (
+                <Button
+                  component={Link}
+                  to="/premium"
+                  className="header__menu-button"
+                >
+                  <span>Premium</span>
+                  <StarBorderOutlinedIcon/>
+                </Button>
+              )}
+              <Button
+                component={Link}
+                to="/dashboard"
                 className="header__menu-button"
               >
-                <span>Premium</span>
-                <StarBorderOutlinedIcon />
+                <span>Dashboard</span>
+                <MonitorHeartOutlinedIcon/>
               </Button>
-            )}
-            <Button
-              component={Link}
-              to="/dashboard"
-              className="header__menu-button"
-            >
-              <span>Dashboard</span>
-              <MonitorHeartOutlinedIcon />
+              <Button
+                component={Link}
+                to="/settings"
+                className="header__menu-button"
+              >
+                <span>{userInfos.name}</span>
+                <AccountCircleOutlinedIcon/>
+              </Button>
+              <Button onClick={() => logout()} className="header__menu-button">
+                <span>Logout</span>
+                <LogoutOutlinedIcon/>
+              </Button>
+            </>
+          ) : (
+            <Button onClick={handleOpen} className="header__menu-button">
+              <span>Connexion</span>
+              <AccountCircleOutlinedIcon/>
             </Button>
-            <Button
-              component={Link}
-              to="/settings"
-              className="header__menu-button"
-            >
-              <span>{userInfos.name}</span>
-              <AccountCircleOutlinedIcon />
-            </Button>
-            <Button onClick={() => logout()} className="header__menu-button">
-              <span>Logout</span>
-              <LogoutOutlinedIcon />
-            </Button>
-          </>
-        ) : (
-          <Button onClick={handleOpen} className="header__menu-button">
-            <span>Connexion</span>
-            <AccountCircleOutlinedIcon />
-          </Button>
-        )}
+          )}
+        </div>
       </div>
-    </div>
-  );
+    )
 };
 
 export default HeaderMenu;
