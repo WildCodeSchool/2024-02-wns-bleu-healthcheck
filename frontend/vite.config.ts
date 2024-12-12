239a96925a8022b495f98739af6dee4fe1307da3
Pierre Castro <pierre.castro89@gmail.com>
2024-12-12 11:51:16 +0100

    chore: Update viteconfig

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,32 +4,35 @@ import path from "path";
 import { UserConfig } from "vitest/config";
 
 export default defineConfig(({ mode }) => {
-  const config: UserConfig = {
-    plugins: [react()],
-    server: {
-      host: true,
-      port: 5173,
-      hmr: {
-        path: "/hmr",
-        port: 7001,
-      },
-    },
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-        "@assets": path.resolve(__dirname, "./src/assets"),
-        "@common": path.resolve(__dirname, "./src/common"),
-        "@styles": path.resolve(__dirname, "./src/common/styles"),
-      },
-    },
-    test: {
-      globals: true,
-      environment: "jsdom",
-    },
-    optimizeDeps: {
-      include: ["@stripe/stripe-js"],  // Ensure Vite pre-bundles this
-    }
-  };
+    const config: UserConfig = {
+        plugins: [react()],
+        server: {
+            host: true,
+            port: 5173,
+            hmr: {
+                path: "/hmr",
+                port: 7001,
+            },
+            watch: {
+                usePolling: true,
+            },
+        },
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+                "@assets": path.resolve(__dirname, "./src/assets"),
+                "@common": path.resolve(__dirname, "./src/common"),
+                "@styles": path.resolve(__dirname, "./src/common/styles"),
+            },
+        },
+        test: {
+            globals: true,
+            environment: "jsdom",
+        },
+        optimizeDeps: {
+            include: ["@stripe/stripe-js"],  // Ensure Vite pre-bundles this
+        }
+    };
 
-  return config;
+    return config;
 });
